96102c1b2e5440941ebfd385de6ea2ed5a59485b
Angel Vargas <99620689+Angelito012@users.noreply.github.com>
2023-03-23 11:46:46 -0600

    Add files via upload

---
+++ Tarea3/src/tarea3/Tarea3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea3;
+
+/**
+ *
+ * @author Angel
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Angel David Vargas Escobar 202102651  IPC G");
+    }
+    
+}
